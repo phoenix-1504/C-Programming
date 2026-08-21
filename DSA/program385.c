bc41039d959d51c8690743e577c55b5f55dc5bba
Piyush Chaudhari <the.piyush.chaudhari@gmail.com>
2026-08-21 14:06:03 +0530

    DSA

---
+++ DSA/program385.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+#pragma pack (1)
+struct node 
+{
+    int data ;
+    struct node *next;
+    struct node *prev; 
+
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+
+int main ()
+{
+    printf("%d\n", sizeof(NODE));   // size of NODE will be 20 bytes
+    
+    return 0;
+
+}
